6e03704b3ecefaf3b1ee8b031dfafaa572f14309
savanna chiang <savannaac@gmail.com>
2021-06-29 22:24:54 -0500

    not done yet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById("create-task-form")
+  const taskInput= document.getElementById("new-task-description")
+  
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+  })
 });
